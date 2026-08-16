f64ababe17d404721643fdcc03455cfe01575a46
Ho Boon How <hoboonhow123@gmail.com>
2026-08-16 10:45:41 +0800

    Add Nelson greeting

--- src/main/java/Nelson.java
+++ src/main/java/Nelson.java
@@ -3,6 +3,24 @@
  */
 public class Nelson {
     public static void main(String[] args) {
-        System.out.println("Nelson");
+        new Nelson().showWelcome();
+    }
+
+    /**
+     * Displays the welcome message when the chatbot starts.
+     */
+    public void showWelcome() {
+        String logo =
+                "    _   __     __\n"
+                        + "   / | / /___ / /________  ____\n"
+                        + "  /  |/ / __ \\/ / ___/ __ \\/ __ \\\n"
+                        + " / /|  /  __/ / (__  ) /_/ / / / /\n"
+                        + "/_/ |_/\\___/_/_/____/\\____/_/ /_/\n";
+
+        System.out.println("System booting...\n" + logo);
+        System.out.println("____________________________________________________________");
+        System.out.println("Molo! I have a surprise for you. Your move!");
+        System.out.println("Type your command, or are you just going to let your time run out?");
+        System.out.println("____________________________________________________________");
     }
 }
