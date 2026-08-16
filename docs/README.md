19df72fcb68e1e1cf1faee9c4320ee6c421b7dfe
Ho Boon How <hoboonhow123@gmail.com>
2026-08-16 10:44:26 +0800

    Rename main class to Nelson

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Nelson User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Nelson.java
@@ -0,0 +1,8 @@
+/**
+ * Starts the Nelson chatbot application.
+ */
+public class Nelson {
+    public static void main(String[] args) {
+        System.out.println("Nelson");
+    }
+}
